508d05ec6928010826e7f40d64c4ce12680c9a21
Mridulbirla13 <mridulbirla13@gmail.com>
2024-06-27 15:49:20 +0530

    update

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/auth/login",{
+      const response = await fetch("https://jwt-node.onrender.com/auth/login",{
         method:"POST",
         headers:{
           "Content-Type":"application/json"
--- src/pages/auth/signup/Signup.js
+++ src/pages/auth/signup/Signup.js
@@ -28,7 +28,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/user/register",{
+      const response = await fetch("https://jwt-node.onrender.com/user/register",{
         method:"POST",
         headers:{
           "Content-Type":"application/json"
